a0713da42bbdf6ffaa2f6254609c526c5fa87504
mbganesh <crazyganesh179@gmail.com>
2020-04-09 11:56:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/day02/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.day02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
